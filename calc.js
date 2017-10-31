31ba59c17d848390e3e78bafd59fb743e45b9007
Moustakime KIFIA <mkifia@icloud.com>
2017-10-31 10:09:49 +0100

    calculator, bug fixed

--- calc.js
+++ calc.js
@@ -1,29 +1,33 @@
 
+function calculatrice () {
+    var oper = prompt('Quel type d\'opération soouhaitez-vous réaliser ? : +, -, *, /');
+    var nbre1 = parseInt(prompt('la première valeur :'));
+    var nbre2 = parseInt(prompt('la deuxième valeur :'));
+    var resultat;
 
-var oper = prompt('Quel type d\'opération soouhaitez-vous réaliser ? : +, -, *, /');
-var nbre1 = parseInt(prompt('la première valeur :'));
-var nbre2 = parseInt(prompt('la deuxième valeur :'));
-var resultat;
+    switch (oper) {
+        case '+':
+            resultat = nbre1 + nbre2;
+            break;
+        case '-':
+            resultat = nbre1 - nbre2;
+            break;
+        case '*':
+            resultat = nbre1 * nbre2;
+            break;
+        case '/':
+            resultat = nbre1 / nbre2;
+            break;
+        default:
+            resultat = null;
+            break;
+    }
 
-switch (oper) {
-    case '+':
-        resultat = nbre1 + nbre2;
-        break;
-    case '-':
-        resultat = nbre1 - nbre2;
-        break;
-    case '*':
-        resultat = nbre1 * nbre2;
-        break;
-    case '/':
-        resultat = nbre1 / nbre2;
-    default:
-        resultat = null;
-        break;
+    if(resultat) {
+        alert(resultat);
+    } else {
+        alert('Type d\'opération inconnu');
+    }
 }
 
-if(resultat) {
-    alert(resultat);
-} else {
-    alert('Type d\'opération inconnu');
-}
+calculatrice();
