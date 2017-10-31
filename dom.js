28fc13932ea9bfc5725abef29d065d1af8f3c1e2
Moustakime KIFIA <mkifia@icloud.com>
2017-10-31 23:52:26 +0100

    jquery

--- dom.js
+++ dom.js
@@ -1,12 +1,7 @@
-var prenom = document.getElementById('prenom');
-var nom = document.getElementById('nom');
-var resultat = document.getElementById('resultat');
-var submit = document.getElementById('submit');
-
-function setResult () {
-    resultat.innerHTML = prenom.value + ' ' + nom.value;
-}
-
-submit.addEventListener('click', function() {
-    setResult();
+$(function(){
+    $('#submit').on('click', function () {
+        var prenom = $('#prenom').val();
+        var nom = $('#nom').val();
+        $('#resultat').val(prenom + ' ' + nom);
+    });
 });
