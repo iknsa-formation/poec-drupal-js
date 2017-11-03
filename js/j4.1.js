d4710563a9fd032012b1a1b1c2639c318fd6016a
Moustakime KIFIA <mkifia@icloud.com>
2017-11-03 14:41:24 +0100

    delete author

--- js/j4.1.js
+++ js/j4.1.js
@@ -1,4 +1,8 @@
 $(function () {
+    listAuthors();
+});
+
+function listAuthors() {
     $.ajax({
         url: 'services/api.php',
         dataType: 'json',
@@ -12,6 +16,9 @@ $(function () {
             tab += '<th>Nom</th>';
             tab += '<th>Prénom</th>';
             tab += '<th>Fonction</th>';
+            tab += '<th>Supprimer</th>';
+            tab += '<th>Modifier</th>';
+            tab += '<th>Voir</th>';
             tab += '</tr></thead>';
 
             tab += '<tbody>';
@@ -20,15 +27,33 @@ $(function () {
                 tab += '<td>' + val.nom + '</td>';
                 tab += '<td>' + val.prenom + '</td>';
                 tab += '<td>' + val.fonction + '</td>';
+                tab += '<td><button onclick="supprimer(' + val.id_auteur + ')" class="btn btn-danger">Supprimer</button></td>';
+                tab += '<td><button onclick="modifier(' + val.id_auteur + ')" class="btn btn-warning">Modifier</button></td>';
+                tab += '<td><button onclick="voir(' + val.id_auteur + ')" class="btn btn-primary">Voir</button></td>';
                 tab += '</tr>';
             });
 
             tab += '</tbody>';
             tab += '</table>';
-            $('#liste-auteurs').append(tab);
+            $('#liste-auteurs').html(tab);
         },
         error: function (error) {
 
         }
     });
-});
+}
+var supprimer = function (id) {
+    $.ajax({
+        url: 'services/api.php',
+        data: {
+            method: 'deleteAuthor',
+            id_auteur: id
+        },
+        success: function () {
+            listAuthors();
+        },
+        error: function () {
+
+        }
+    });
+};
