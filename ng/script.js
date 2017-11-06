3cd3349d8db6d93392ff947a39b1892080883ce3
Moustakime KIFIA <mkifia@icloud.com>
2017-11-06 17:07:13 +0100

    todo list

--- ng/script.js
+++ ng/script.js
@@ -1,14 +1,25 @@
 var app = angular.module('appPoec', []);
 
-app.controller('poecCtrl', function ($scope) {
-    $scope.user = {
-        name: 'Sebastien',
-        lastname: 'Oliver'
+app.controller('poecCtrl', function ($scope, $http) {
+    $scope.todoList = [];
+
+    $scope.add = function () {
+        var newElement = {
+            name: $scope.newTodo,
+            done: false
+        };
+        $scope.todoList.push(newElement);
+        $scope.newTodo = '';
     };
 
-    $scope.addition = function () {
-        alert('addition');
-        $scope.res = parseInt($scope.a) + parseInt($scope.b);
+    $scope.archiver = function () {
+        var tmp = [];
+        angular.forEach($scope.todoList, function (todo) {
+            if (!todo.done) {
+                tmp.push(todo);
+            }
+        });
+        $scope.todoList = tmp;
     };
 });
 
